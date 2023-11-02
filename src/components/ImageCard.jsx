a68dd5096b963cadbe2c6b16fc57da8dbfc6c030
taimul <taimul.haque@gmail.com>
2023-11-02 15:06:08 +0600

    input hover effect

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -5,22 +5,26 @@ const ImageCard = ({ id, selected, image, index, handleSelection }) => {
 
   return (
     <div
-      className={` relative group overflow-hidden ${
+      className={` relative group ${
         index === 0 ? "col-span-2 row-span-2" : "col-span-1 row-span-1"
       }`}
     >
-      <img
-        src={image}
-        alt=""
-        className={`border rounded-lg ${selected ? "opacity-50" : ""}`}
-      />
+      <div className="relative overflow-hidden rounded-lg">
+        <img
+          src={image}
+          alt=""
+          className={`border-2  ${selected ? "opacity-50" : ""}`}
+        />
+        <div className="h-80 w-80 bg-gray-900 opacity-0 group-hover:opacity-50 absolute top-0 "></div>
+      </div>
       <input
         onChange={toggleSelected}
         type="checkbox"
-        className="absolute top-3 left-3 z-20"
+        className={`absolute top-3 left-3 z-20 opacity-0 group-hover:opacity-100 ${
+          selected ? "opacity-100" : ""
+        }`}
         checked={selected}
       />
-      <div className="h-80 w-80 bg-gray-900 opacity-0 group-hover:opacity-50 absolute top-0"></div>
     </div>
   );
 };
