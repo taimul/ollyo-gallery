9036c326d6fe5bbcb322902c9e748b5dd4e5659d
taimul <taimul.haque@gmail.com>
2023-11-02 18:35:14 +0600

    drag added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import datas from "./data/data.json";
 import ImageCard from "./components/ImageCard";
 import { useState } from "react";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
 
 function App() {
   const [data, setData] = useState(datas);
@@ -23,10 +29,24 @@ function App() {
     setSelectedImages([]);
   };
 
+  // Handle drag and drop
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id !== over.id) {
+      setData((items) => {
+        const preIndex = items.findIndex((item) => item.id === active.id);
+        const nextIndex = items.findIndex((item) => item.id === over.id);
+
+        return arrayMove(items, preIndex, nextIndex);
+      });
+    }
+  };
+
   return (
     <div className="bg-slate-200 h-screen">
       <div className="flex justify-center items-center h-screen">
-        <div className="bg-white w-[600px]">
+        <div className="bg-white w-[800px]">
           <div>
             <div className="flex justify-between mx-5 py-3">
               <div>
@@ -47,28 +67,39 @@ function App() {
                 )}
               </div>
               {selectedImages.length > 0 && (
-                <button onClick={handleDelete} className="text-red-600">
+                <button
+                  onClick={handleDelete}
+                  className="text-red-600 font-bold text-xs"
+                >
                   {selectedImages.length > 1 ? (
-                    <span>Delete Files</span>
+                    <span>Delete files</span>
                   ) : (
-                    <span>Delete File</span>
+                    <span>Delete file</span>
                   )}
                 </button>
               )}
             </div>
             <hr />
-            <div className="grid grid-cols-5 gap-2 p-6">
-              {data.map((imgData, index) => (
-                <ImageCard
-                  key={imgData.id}
-                  id={imgData.id}
-                  image={imgData.image}
-                  index={index}
-                  handleSelection={handleSelectionChange}
-                  selected={selectedImages.includes(imgData.id)}
-                />
-              ))}
-            </div>
+
+            <DndContext
+              collisionDetection={closestCenter}
+              onDragEnd={handleDragEnd}
+            >
+              <SortableContext items={data} strategy={rectSortingStrategy}>
+                <div className="grid grid-cols-5 gap-2 p-6">
+                  {data.map((imgData, index) => (
+                    <ImageCard
+                      key={imgData.id}
+                      id={imgData.id}
+                      image={imgData.image}
+                      index={index}
+                      handleSelection={handleSelectionChange}
+                      selected={selectedImages.includes(imgData.id)}
+                    />
+                  ))}
+                </div>
+              </SortableContext>
+            </DndContext>
           </div>
         </div>
       </div>
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,26 +1,50 @@
-const ImageCard = ({ id, selected, image, index, handleSelection }) => {
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+const ImageCard = (props) => {
+  const { id, selected, image, index, handleSelection } = props;
+
+  const sortable = useSortable({ id });
+
+  const { attributes, listeners, setNodeRef, transform, transition } = sortable;
+  const style = {
+    // transform: transform ? `translate(${transform.x}px, ${transform.y}px` : "",
+    transform: CSS.Transform.toString(transform),
+
+    transition,
+  };
+
   const toggleSelected = () => {
     handleSelection(!selected, id);
   };
 
   return (
     <div
-      className={` relative group ${
-        index === 0 ? "col-span-2 row-span-2" : "col-span-1 row-span-1"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      className={` relative group z-30 overflow-auto ${
+        index === 0 ? "col-span-2 row-span-2" : null
       }`}
     >
-      <div className="relative overflow-hidden rounded-lg">
+      <div {...listeners} className="relative overflow-hidden rounded-lg">
         <img
           src={image}
           alt=""
-          className={`border-2  ${selected ? "opacity-50" : ""}`}
+          className={`border-2 ${selected ? "opacity-50" : ""}`}
         />
-        <div className="h-80 w-80 bg-gray-900 opacity-0 group-hover:opacity-50 absolute top-0 "></div>
+        <div
+          className={`${
+            selected
+              ? ""
+              : "h-80 w-80 bg-gray-900 opacity-0 group-hover:opacity-50 absolute top-0 "
+          }`}
+        ></div>
       </div>
       <input
         onChange={toggleSelected}
         type="checkbox"
-        className={`absolute top-3 left-3 z-20 opacity-0 group-hover:opacity-100 ${
+        className={`absolute top-3 left-3  opacity-0 group-hover:opacity-100 ${
           selected ? "opacity-100" : ""
         }`}
         checked={selected}
